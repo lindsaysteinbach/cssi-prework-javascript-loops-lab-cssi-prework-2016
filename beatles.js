50883813da58e418d010a4a9a64eff7c26ba9689
lindsaysteinbach <lsteinbach@google.com>
2016-05-04 12:19:40 -0400

    Done.

--- beatles.js
+++ beatles.js
@@ -1,1 +1,33 @@
 'use strict';
+
+var i = 0;
+function johnLennonFacts(facts) {
+  while (i < facts.length) {
+    var newFact = facts[i] + "!!!";
+    excitingFacts.push(newFact);
+    i++;
+  }
+  return excitingFacts;
+}
+
+var combinedArray = [];
+
+function theBeatlesPlay(musicians,instruments) {
+	for (var i = 0;  i < musicians.length; i++) {
+    var string = musicians[i] + " plays " + instruments[i];
+    combinedArray.push(string);
+  }
+  return combinedArray;
+}
+
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  var i = 0;
+  do {
+    array.push("I love the Beatles!")
+    i++;
+  }
+  while (i < number && i < 10);
+  return array;
+}
